58afd6e20689e321dfcb907f97b17de32651ef9d
KalKya <nachapon200217@gmail.com>
2022-07-24 18:28:51 +0700

    new commit

--- utility.js
+++ utility.js
@@ -4,9 +4,10 @@ const request = require("request-promise");
 const EXTERNAL_API=""; // put url
 const accessToken=""; // put access token
 const student = {
-  name: '', // replace with your full name.
-  age: 19, // put your age.
-  gender: '', // replace with your gender
+  student_id: 63361696,
+  name: 'Nachapon Norrarat', // replace with your full name.
+  age: 20, // put your age.
+  gender: 'Male', // replace with your gender
   department: 'CPE'
 };
 
